73cee95bd688346ffda6f39f481df4b707d46c59
Corentin Beuchet <corentin.beuchet@hotmail.fr>
2026-01-15 19:25:05 +0100

    chore: init project

---
+++ src/test/java/ort/_olen/course/TestCourseApplication.java
@@ -0,0 +1,11 @@
+package ort._olen.course;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CourseApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
